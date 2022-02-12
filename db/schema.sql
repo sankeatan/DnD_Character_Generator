0774e9286bcfadd55a47f01057b6559c926ff28d
Damon Carver <dmcarver0611@gmail.com>
2022-02-12 15:51:45 -0500

    added character page

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,22 @@
 CREATE DATABASE dndCharacterGenerator_db;
 
 CREATE TABLE User (
-   id INT NOT NULL,
-   name varchar(255) NOT NULL,
-   email varchar(255) NOT NULL,
-   password varchar(30) NOT NULL, 
-   primary key (id)
- );
+  id INT NOT NULL,
+  name varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  password varchar(30) NOT NULL, 
+  primary key (id)
+);
 
- CREATE TABLE Character (
-   id INT NOT NULL,
-   name varchar(255) NOT NULL,
-   description varchar(255) NOT NULL,
-   lvl INT NOT NULL,
-   race varchar(255) NOT NULL,
-   class varchar(255) NOT NULL,
-   subclass varchar(255) NOT NULL,
-   background varchar(255), 
-   foreign key (userId)
- );
+CREATE TABLE Character (
+  id INT NOT NULL,
+  name varchar(255) NOT NULL,
+  description varchar(255) NOT NULL,
+  lvl INT NOT NULL,
+  race varchar(255) NOT NULL,
+  class varchar(255) NOT NULL,
+  subclass varchar(255) NOT NULL,
+  background varchar(255), 
+  foreign key (userId)
+);
 
