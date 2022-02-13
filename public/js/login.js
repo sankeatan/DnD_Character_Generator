0091c6e491259201b119864fc72119e91f66e393
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-02-13 12:42:23 -0500

    seperated login and signup js

--- public/js/login.js
+++ public/js/login.js
@@ -22,42 +22,7 @@ const loginFormHandler = async (event) => {
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const name = document.querySelector('#name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-  const confirmation = document.querySelector('#password-confirmation').value.trime();
-  if (password !== confirmation){
-    alert("Passwords do not match");
-  }
-  else if (name && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ name, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      const loginResponse = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-      if (loginResponse.ok){
-        document.location.replace('/');
-      } else {loginResponse.statusText}
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
 
 document
   .querySelector('#login-form')
   .addEventListener('submit', loginFormHandler);
-
-document
-  .querySelector('#signup-form')
-  .addEventListener('submit', signupFormHandler);
