9a30447c19fa4405e3c25a957a53172bb4c63091
William Gartman <wngartman@gmail.com>
2022-02-15 06:05:51 -0600

    Getting info from db

--- controllers/api/classesRoutes.js
+++ controllers/api/classesRoutes.js
@@ -3,44 +3,50 @@ const axios = require('axios');
 const ClassDesc = require('../../models/ClassDesc');
 
 router.get('/', async (req, res) => {
-    await axios.get('https://www.dnd5eapi.co/api/classes')
-  .then(function (response) {
-    // handle success
-    console.log(response.data.results);
-    res.json(response.data.results);
-    const classes = response.data.results;
-    return classes;
-    
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .then(function () {
-  });
+  await axios.get('https://www.dnd5eapi.co/api/classes')
+    .then(function (response) {
+      // handle success
+      console.log(response.data.results);
+      res.json(response.data.results);
+      const classes = response.data.results;
+      return classes;
+
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function () {
+    });
 });
 
 router.get('/:class', async (req, res) => {
   lowercaseSearch = (req.params.class).toLowerCase();
   const apiClassCall = `https://www.dnd5eapi.co/api/classes/${lowercaseSearch}`;
-  console.log(apiClassCall);
-  await axios.get(apiClassCall) 
+  await axios.get(apiClassCall)
   .then(function (response) {
-    const classCall = ClassDesc.findOne({ where: { class: req.params.class } })
-    //classDescription = classCall.json();
-    console.log(classCall);
-    const classes = response;
+      console.log(apiClassCall);
+      const classCall = ClassDesc.findOne({ where: { class: req.params.class } })
+      //classDescription = classCall.json();
+      console.log(classCall);
+      const classes = response;
 
-  return classes;
-})
+      classCall.then(function (result) {
+        response.data.description = result.dataValues.description
+
+        // console.log(result.dataValues.description)
+        console.log(response.data)
+        // return classes;
+      })
+    })
 
 
-.catch(function (error) {
-  // handle error
-  console.log(error);
-})
-.then(function () {
-  // always executed
-});
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
 });
 
 module.exports = router;
