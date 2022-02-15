f50dd533097fbe874a786ddba16fac411504e783
William Gartman <wngartman@gmail.com>
2022-02-15 01:16:12 -0600

    dumb

--- public/js/character-builder.js
+++ public/js/character-builder.js
@@ -74,10 +74,6 @@ function raceDropDown() {
     selection = choices.find(".option-Btn").on('click', inputChoice);
 }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> eb105aec2d041ed47c3f6c8f0a3349d31404b8e6
 function backgroundDropDown() {
     choices.empty();
     view = "background";
