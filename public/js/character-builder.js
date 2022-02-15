200e9b9db0a08dcc4969d47d16cebae2d656cbd4
William Gartman <wngartman@gmail.com>
2022-02-14 21:24:29 -0600

    edit

--- public/js/character-builder.js
+++ public/js/character-builder.js
@@ -23,20 +23,20 @@ var view = '';
 
 function show(value) {
     document.querySelector(".text-box").value = value;
-  }
-  
-  let dropdown = document.querySelector(".dropdown")
-  dropdown.onclick = function() {
-      dropdown.classList.toggle("active")
-      console.log()
-  }
+}
 
-function classDropDown(){
+let dropdown = document.querySelector(".dropdown")
+dropdown.onclick = function () {
+    dropdown.classList.toggle("active")
+    console.log()
+}
+
+function classDropDown() {
     choices.empty()
     view = "class";
     console.log(view);
     const classArray = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard']
-    for (var i = 0; i<classArray.length; i++){
+    for (var i = 0; i < classArray.length; i++) {
         const imageUrl = `./images/${classArray[i]}Icon.png`;
         var optionDiv = $('<div>').addClass('option-Div')
         var optionImg = $('<img>').addClass('option-Img').attr("src", imageUrl)
@@ -47,12 +47,12 @@ function classDropDown(){
     }
 }
 
-function raceDropDown(){
+function raceDropDown() {
     choices.empty();
     view = "race";
     console.log(view);
     const raceArray = ['Dragonborn', 'Dwarf', 'Elf', 'Gnome', 'Half-Elf', 'Halfling', 'Half-Orc', 'Human', 'Tiefling'];
-    for (var i = 0; i < raceArray.length; i++){
+    for (var i = 0; i < raceArray.length; i++) {
         const imageUrl = `./images/${raceArray[i]}Icon.png`;
         var optionDiv = $('<div>').addClass('option-Div')
         var optionImg = $('<img>').addClass('option-Img').attr("href", imageUrl)
@@ -63,18 +63,18 @@ function raceDropDown(){
     }
 }
 
-function inputChoice(){
+function inputChoice() {
     console.log('click');
     switch (view) {
-        case "class": 
-        console.log($(this).val())
-        newCharacter.class == $(this).val();
-        raceDisplay.text(newCharacter.class);
+        case "class":
+            console.log($(this).val())
+            newCharacter.class == $(this).val();
+            raceDisplay.text(newCharacter.class);
             break;
-        case "view": 
-        console.log($(this).val())
-        newCharacter.race == $(this).val();
-        classDisplay.text(newCharacter.race);
+        case "view":
+            console.log($(this).val())
+            newCharacter.race == $(this).val();
+            classDisplay.text(newCharacter.race);
             break;
     }
 }
@@ -83,10 +83,10 @@ const statArray = [15, 14, 13, 12, 10, 8];
 
 raceDropDown();
 
-choices.find(".option-Btn").on('click', inputChoice);
 
 classOptions.on('click', classDropDown);
 raceOptions.on('click', raceDropDown);
+choices.find(".option-Btn").on('click', inputChoice);
 
 
 
